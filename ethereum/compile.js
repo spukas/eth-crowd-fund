f9c4bceb779bd641b4d98734b87b0e3d545111e4
Linas Spukas <l.spukas@gmail.com>
2018-03-08 22:13:10 +0200

    Compile Campaign contract using solc

---
+++ ethereum/compile.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+
+// remove build folder for new contracts in the future
+fs.removeSync(buildPath);
+
+const source = fs.readFileSync(campaignPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+// create build folder to hold compiled contracts
+fs.ensureDirSync(buildPath);
+
+// loop through all possible contracts and compile each
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, `${contract}.json`),
+    output[contract],
+  );
+}
