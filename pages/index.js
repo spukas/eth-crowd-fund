8f2890f0a1adc0c6c47e10d1a0820ca1a160d9ad
Linas Spukas <l.spukas@gmail.com>
2018-03-20 22:45:13 +0200

    Replace data fetchign from componentDidMount() to getInitialProps() to fetch data on server

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
-    const campaignList = await factory.methods.getDeployedCampaigns().call();
+  // this is specific next.js method to load data on the server
+  // Next.js ignores componendDidMount() on the server
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-    console.log({ campaignList });
+    // return an object that will be passed as props to the component
+    return { campaigns };
   }
 
+  renderCampaigns = () =>
+    this.props.campaigns.map(campaign => (
+      <li key={campaign}>{`Campaign active on: ${campaign}`}</li>
+    ));
+
   render() {
-    return <div>Campaign list:</div>;
+    return (
+      <div>
+        Campaign list:
+        <ul>{this.renderCampaigns()}</ul>
+      </div>
+    );
   }
 }
 
