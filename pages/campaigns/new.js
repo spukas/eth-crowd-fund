4922bc6c4e404ea4542c7e898822bbeb4c48122c
Linas Spukas <l.spukas@gmail.com>
2018-03-26 21:32:41 +0200

    Create form for adding new campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { Form } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
-  state = {};
+  state = { minContribution: '', submitedContribution: '' };
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+
+  handleSubmit = () => {
+    const { minContribution } = this.state;
+
+    this.setState({
+      submitedContribution: minContribution,
+      minContribution: '',
+    });
+  };
 
   render() {
+    const { minContribution, submitedContribution } = this.state;
+
     return (
       <Layout>
-        <h1>newCampaign component</h1>
+        <h3>Create new Campaign</h3>
+        <Form onSubmit={this.handleSubmit}>
+          <label htmlFor="minContribution">Minimum contribution</label>
+          <Form.Input
+            name="minContribution"
+            value={minContribution}
+            onChange={this.handleChange}
+          />
+          <Form.Button content="Create" primary />
+        </Form>
+        <strong>onChange:</strong>
+        <pre>{JSON.stringify({ minContribution }, null, 2)}</pre>
+        <strong>onSubmit:</strong>
+        <pre>{JSON.stringify({ submitedContribution }, null, 2)}</pre>
       </Layout>
     );
   }
