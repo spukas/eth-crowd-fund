8cd601f2298d302e996ead0680faf1545c012451
Linas Spukas <l.spukas@gmail.com>
2018-03-27 22:12:31 +0200

    Hook up createCampaign method from factory contract

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,40 +1,67 @@
 import React, { Component } from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
-  state = { minContribution: '', submitedContribution: '' };
+  state = {
+    minContribution: '',
+    submitedContribution: '',
+    submittedFromAccount: '',
+  };
 
   handleChange = (e, { name, value }) => this.setState({ [name]: value });
 
-  handleSubmit = () => {
+  handleSubmit = async event => {
+    event.preventDefault();
     const { minContribution } = this.state;
+
+    const accounts = await web3.eth.getAccounts();
+    await factory.methods
+      .createCampaign(minContribution)
+      .send({ from: accounts[0] });
 
     this.setState({
       submitedContribution: minContribution,
+      submittedFromAccount: accounts[0],
       minContribution: '',
     });
   };
 
   render() {
-    const { minContribution, submitedContribution } = this.state;
+    const {
+      minContribution,
+      submitedContribution,
+      submittedFromAccount,
+    } = this.state;
 
     return (
       <Layout>
         <h3>Create new Campaign</h3>
         <Form onSubmit={this.handleSubmit}>
-          <label htmlFor="minContribution">Minimum contribution</label>
-          <Form.Input
-            name="minContribution"
-            value={minContribution}
-            onChange={this.handleChange}
-          />
+          <Form.Field>
+            <label htmlFor="minContribution">Minimum contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              name="minContribution"
+              value={minContribution}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
           <Form.Button content="Create" primary />
         </Form>
         <strong>onChange:</strong>
         <pre>{JSON.stringify({ minContribution }, null, 2)}</pre>
         <strong>onSubmit:</strong>
-        <pre>{JSON.stringify({ submitedContribution }, null, 2)}</pre>
+        <pre>
+          {JSON.stringify(
+            { submitedContribution, submittedFromAccount },
+            null,
+            2,
+          )}
+        </pre>
       </Layout>
     );
   }
