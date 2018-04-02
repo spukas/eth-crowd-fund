5b926575f426546c1ceec2ca6ba74a6f6d4fd74f
Linas Spukas <l.spukas@gmail.com>
2018-04-02 10:05:53 +0300

    Update Campaign contract by adding summary helpers; Compile and redeploy

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xfe35FF00E18e8b3a7268cfE67b0447f776349B73',
+  '0x70379E1C228Cc42F887CfFE0a29eC08D35b4E07D',
 );
 
 export default instance;
