097d5cf9fb33cbe6daef586e9f07a6110e69249c
Linas Spukas <l.spukas@gmail.com>
2018-04-15 22:05:40 +0300

    Build table to show all requests with details

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import Layout from '../../../components/Layout';
 import { Link } from '../../../routes';
 import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
 
 export default class RequestsIndex extends Component {
     static async getInitialProps({ query: { address } }) {
         const campaign = Campaign(address);
-        const { methods: { getRequestCount, requests: getRequests } } = campaign;
+        const { methods: { getRequestCount, requests: getRequests, approversCount } } = campaign;
         const requestsCount = await getRequestCount().call();
+        const approvers = await approversCount().call();
 
         const requests = await Promise.all(
-            [...Array(parseInt(requestsCount))]
-                .map((_, index) => getRequests(index).call()),
+            [...Array(parseInt(requestsCount))].map((_, index) => getRequests(index).call()),
         );
 
-        return { address, requests, requestsCount };
+        return { address, requests, requestsCount, approvers };
     }
 
+    renderRequests = () => {
+        const { Row, Cell } = Table;
+        const { approvers } = this.props;
+        return this.props.requests.map(
+            ({ description, value, recipient, approvalCount, complete }, index) => (
+                <Row key={`${index}-${description}`}>
+                    <Cell>{index + 1}</Cell>
+                    <Cell>{description}</Cell>
+                    <Cell>{web3.utils.fromWei(value, 'ether')}</Cell>
+                    <Cell>{recipient}</Cell>
+                    <Cell>{`${approvalCount} / ${approvers}`}</Cell>
+                    <Cell>
+                        <Button primary content="Approve" />
+                    </Cell>
+                    <Cell>
+                        <Button primary content="Finalize" />
+                    </Cell>
+                </Row>
+            ),
+        );
+    };
     render() {
         const { address } = this.props;
+        const { Body, Header, HeaderCell, Row, Cell } = Table;
 
         return (
             <Layout>
@@ -27,6 +50,23 @@ export default class RequestsIndex extends Component {
                     <a>Go back</a>
                 </Link>
                 <h3>Requests List</h3>
+
+                <Table celled>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approval count</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+
+                    <Body>{this.renderRequests()}</Body>
+                </Table>
+
                 <Link route={`/campaigns/${address}/requests/new`}>
                     <a>
                         <Button primary content="Add Rrequest" />
